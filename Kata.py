14245f12d8ee7cb708c8932ee92f9a99610d5120
Your Name <artger@ukr.net>
2020-01-14 18:42:39 +0200

    Tetris Series #1 — Scoring System (Python)

--- Kata.py
+++ Kata.py
@@ -19,5 +19,23 @@ def solution(number):
             result += i
 
     return result
+
+
+Tetris Series #1 — Scoring System (Python)
+
+    def get_score(arr) -> int:
+    lvl = 1
+    lines = 0
+    score = 0
+    bal = {0:0, 1:40, 2:100, 3:300, 4:1200}
+    for i in arr:
+        if lines >= 10:
+            lines -= 10
+            lvl += 1
+        lines += i
+        score += bal[i]*lvl
+        #print(f"i={i} lines={lines} bal[i]={bal[i]} lvl={lvl} score={score} ")
+
+    return score
     """
 
