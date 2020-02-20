797efcbbce64377300654bb306a8cc5b80bf0774
Piikasooo <39100330+Piikasooo@users.noreply.github.com>
2020-02-20 11:58:23 +0200

    Selective fear of numbers (Python)

--- Kata.py
+++ Kata.py
@@ -385,4 +385,18 @@ def lottery(s):
     new = n // 2
     if n % 2: return fusc(new) + fusc(new + 1)
     return fusc(new)
+    
+    
+    
+   Selective fear of numbers (Python) 
+    
+    def am_I_afraid(day,num):
+    if day == 'Monday' and num == 12: return True
+    if day == 'Tuesday' and num > 95:return True
+    if day == 'Wednesday' and num == 34: return True
+    if day == 'Thursday' and num == 0: return True
+    if day == 'Friday' and num%2 == 0: return True
+    if day == 'Saturday' and num == 56: return True
+    if day == 'Sunday' and (num == 666 or num == -666): return True
+    return False
 """
